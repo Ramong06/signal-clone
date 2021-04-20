384ad834d40b6cb6aa6a554c9d484acf80a6808c
RG-Mac <ramong5406@gmail.com>
2021-04-20 12:34:15 -0500

    added react-navigation to app

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ2T_CTi9c1BA5CasrdrTqU1qslLHiI5U",
+  authDomain: "signal-clone-ae1b8.firebaseapp.com",
+  projectId: "signal-clone-ae1b8",
+  storageBucket: "signal-clone-ae1b8.appspot.com",
+  messagingSenderId: "328744698312",
+  appId: "1:328744698312:web:4b1657f96bd7f4f75f00fd"
+};
